0e2bc0f69b9327b4ab657d74aefca56589e3925f
Thiago Melato <103614164+Melatoo@users.noreply.github.com>
2026-09-24 18:46:49 -0300

    fix(web): remove o LayoutShell duplicado da home (#34)

--- missahoje-web/src/app/page.tsx
+++ missahoje-web/src/app/page.tsx
@@ -1,12 +1,9 @@
-import { LayoutShell } from '../components/layouts/LayoutShell';
 import { MapComponent } from '../features/map/components/MapComponent';
 
 export default function Home() {
   return (
-    <LayoutShell>
-      <div className="absolute inset-0 w-full h-full">
-        <MapComponent />
-      </div>
-    </LayoutShell>
+    <div className="absolute inset-0 w-full h-full">
+      <MapComponent />
+    </div>
   );
 }
